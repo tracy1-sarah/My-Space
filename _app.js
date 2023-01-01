041e5925dd1da4516fa571b8e478ed6005b5d548
Tracy Sarah Afram-Owusu <52568336+tracy1-sarah@users.noreply.github.com>
2023-01-01 10:09:11 -0800

    Add files via upload

---
+++ _app.js
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+import { motion } from "framer-motion";
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div 
+          key={router.route}
+    initial="initial"
+      animate="animate"
+      transition={{ duration: 0.2 }}
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}>
+    <Component {...pageProps} />
+  </motion.div>
+  )
+}
+
+export default MyApp
